18563b1455a1fdfadba2ff72a5aa32075d03b6a0
Ben Anhalt <anhalt@ku.edu>
2014-04-30 15:22:01 -0500

    change rectangle representation and add stroke-width

--- CTM.hs
+++ CTM.hs
@@ -1,15 +1,14 @@
 import Text.XML.Light
 
-data Rectangle = Rectangle Double Double Double Double
-               deriving (Show, Eq)
-
-data Surface = Surface Double Double Double Double
-             deriving (Show, Eq)
+data Rectangle = Rectangle {
+  rectangleX,
+  rectangleY,
+  rectangleW,
+  rectangleH,
+  rectangleB :: Double
+  } deriving (Show, Eq)
 
 data Dir = X | Y deriving (Show, Eq)
-
-data Cushion = Cushion Rectangle Surface
-             deriving (Show, Eq)
 
 data Tree = Tree Double [Tree]
           deriving (Show)
@@ -17,12 +16,12 @@ data Tree = Tree Double [Tree]
 treeSize (Tree size _) = size
 
 partitionRect :: Dir -> Rectangle -> Double -> [Double] -> [Rectangle]
-partitionRect d (Rectangle x0 y0 x1 y1) s0 ss = tail $ scanl (shiftRect d) r0 ws
-  where r0 = if d == X then Rectangle 0 y0 x0 y1 else Rectangle x0 0 x1 y0
-        w  = if d == X then x1 - x0 else y1 - y0
-        ws = map (\s -> w*s/s0) ss
-        shiftRect X (Rectangle _ _ x _) w = Rectangle x  y0 (x + w) y1
-        shiftRect Y (Rectangle _ _ _ y) w = Rectangle x0 y  x1      (y + w)
+partitionRect d (Rectangle x y w h b) s0 ss = tail $ scanl (shiftRect d) r0 ws
+  where r0 = if d == X then Rectangle 0 y x h b else Rectangle x 0 w y b
+        w0  = if d == X then w else h
+        ws = map (\s -> w0*s/s0) ss
+        shiftRect X (Rectangle x y w h b) w' = Rectangle (x + w) y       w' h   (0.5*b)
+        shiftRect Y (Rectangle x y w h b) h' = Rectangle x       (y + h) w  h'  (0.5*b)
 
 ctm :: Dir -> Rectangle -> Tree -> [Rectangle]
 ctm d r (Tree _ []) = [r]
@@ -33,13 +32,14 @@ ctm d r (Tree size children) = r : (concat $ map (\(r, c) -> ctm d' r c) (zip rs
 
 
 svgRectangle :: Rectangle -> Element
-svgRectangle (Rectangle x0 y0 x1 y1) = unode "rect" [Attr (unqual "x") (show x0),
-                                                     Attr (unqual "y") (show y0),
-                                                     Attr (unqual "width") (show (x1 - x0)),
-                                                     Attr (unqual "height") (show (y1 - y0)),
-                                                     Attr (unqual "stroke") "black",
-                                                     Attr (unqual "fill") "white"]
+svgRectangle (Rectangle x y w h b) = unode "rect" [Attr (unqual "x") (show x),
+                                                   Attr (unqual "y") (show y),
+                                                   Attr (unqual "width") (show w),
+                                                   Attr (unqual "height") (show h),
+                                                   Attr (unqual "stroke-width") (show b),
+                                                   Attr (unqual "stroke") "white",
+                                                   Attr (unqual "fill") "none"]
 svgCtm :: Double -> Double -> Tree -> Element
 svgCtm width height tree = unode "svg" ([Attr (unqual "width") (show width),
                                         Attr (unqual "height") (show height)],
-                                        (map svgRectangle $ ctm X (Rectangle 0 0 width height) tree))
+                                        (map svgRectangle $ ctm X (Rectangle 0 0 width height 10) tree))
