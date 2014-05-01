0b6a7ea01c58936ef7d8c999f996ea1d3db7cf71
Ben Anhalt <anhalt@ku.edu>
2014-05-01 16:54:25 -0500

    better svg

--- CTM.hs
+++ CTM.hs
@@ -1,7 +1,11 @@
 module CTM where
 
+import Text.Printf
 import Data.List
 import Text.XML.Light
+
+colors = ["White", "Silver", "Gray", "Red", "Maroon", "Yellow", "Olive",
+          "Lime", "Green", "Aqua", "Teal", "Blue", "Navy", "Fuchsia", "Purple"]
 
 data Rectangle = Rectangle {
   rectangleX,
@@ -27,7 +31,7 @@ partitionRect d (Rectangle x y w h b) s0 ss = tail $ scanl (shiftRect d) r0 ws
         shiftRect Y (Rectangle x y w h b) h' = Rectangle x       (y + h) w  h'  (0.5*b)
 
 ctm :: Dir -> Rectangle -> Tree -> [Rectangle]
-ctm _ _ (Tree 0 _) = []
+ctm _ _ (Tree 0  _) = []
 ctm d r (Tree _ []) = [r]
 ctm d r (Tree size children) = r : (concat $ map (\(r, c) -> ctm d' r c) (zip rs children))
   where ss = map treeSize children
@@ -35,19 +39,25 @@ ctm d r (Tree size children) = r : (concat $ map (\(r, c) -> ctm d' r c) (zip rs
         d' = if d == X then Y else X
 
 
-svgRectangle :: Rectangle -> Element
-svgRectangle (Rectangle x y w h b) = unode "rect" [Attr (unqual "x") (show x),
-                                                   Attr (unqual "y") (show y),
-                                                   Attr (unqual "width") (show w),
-                                                   Attr (unqual "height") (show h),
-                                                   Attr (unqual "stroke-width") "1",
-                                                   Attr (unqual "stroke") "black",
-                                                   Attr (unqual "fill") "none"]
-                                                   -- Attr (unqual "fill-opacity") "0.3"]
+svgRectangle :: String -> Rectangle -> Element
+svgRectangle color (Rectangle x y w h b) =
+  unode "rect" [Attr (unqual "x") (printf "%.2f" x),
+                Attr (unqual "y") (printf "%.2f" y),
+                Attr (unqual "width") (printf "%.2f" w),
+                Attr (unqual "height") (printf "%.2f" h),
+                -- Attr (unqual "stroke-width") "1",
+                -- Attr (unqual "stroke") "black",
+                Attr (unqual "fill") color
+                -- Attr (unqual "fill-opacity") "0.2"
+               ]
+
 svgCtm :: Double -> Double -> Tree -> Element
-svgCtm width height tree = unode "svg" ([Attr (unqual "width") (show width),
-                                        Attr (unqual "height") (show height)],
-                                        (map svgRectangle $
+svgCtm width height tree = unode "svg" ([Attr (unqual "xmlns") "http://www.w3.org/2000/svg",
+                                         Attr (unqual "width") (printf "%.0fpx" width),
+                                         Attr (unqual "height") (printf "%.0fpx" height)],
+                                        (map (\(color, rect) -> svgRectangle color rect) $
+                                         zip (cycle colors) $
                                          sortBy borderSort $
+                                         filter (\(Rectangle _ _ w h _) -> w >= 4 && h >= 4) $
                                          ctm X (Rectangle 0 0 width height 1) tree))
   where borderSort r0 r1 = compare (rectangleB r1) (rectangleB r0)
